e95d85370cbe8d8bd386ada1bfbf91be6493c31b
CesarLoor <ciloor2@espe.edu.ec>
2024-11-22 17:36:46 -0500

    change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'; //importacion de react para trabajar con componentes de react
+
+// se usa para manejar la navegacion entre paginas de react
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
+
+import Reservalist from '../src/components/Reservalist';
+import ReservaForm from '../src/components/ReservaForms';
+// import NotFound from './components/NotFound';
+
+import axios from 'axios';
+
+// definir la aplicacion 
+
+// Router: habilita el enrutamiento
+// Routes: contiene todas las rutas de la aplicacion
+// Route: pat"/" muestra el componente reserva list
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter basename="/">
+
+      <Routes>
+        <Route path="/reservas" element={<Reservalist />} />
+        <Route path = "/nueva" element = {<ReservaForm/>}/>
+          <Route path = "/editar/:id" element = {<ReservaForm/>}/> 
+           {/* <Route path='/*' element={<NotFound />} /> / */}
+      </Routes>
+
+    </BrowserRouter>
   );
 }
 
-export default App;
+export default App;
